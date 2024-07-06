03ed5427451996fb204f47b8641872bff3c5d5cc
Abdullah Faqih Al Mubarak <82459624+fikiham@users.noreply.github.com>
2024-07-06 16:52:59 +0700

    fix: build

--- Assets/Scripts/VfxShooter.cs
+++ Assets/Scripts/VfxShooter.cs
@@ -1,9 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
-using static UnityEngine.GraphicsBuffer;
-using static UnityEngine.Rendering.DebugUI.Table;
 
 public class VfxShooter : MonoBehaviour
 {
